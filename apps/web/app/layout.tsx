7f9f72f5a0081d85b3ef2a733abd1e2d06f9abcb
Zayyan Faizal <3940879+zfaizal2@users.noreply.github.com>
2025-03-31 22:16:19 -0500

    add dynamic packages and setup ui auth

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -3,6 +3,9 @@ import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 
 import './globals.css';
+import { DynamicWidget } from '@dynamic-labs/sdk-react-core';
+import { DynamicContextProvider } from '@dynamic-labs/sdk-react-core';
+import { SolanaWalletConnectors } from "@dynamic-labs/solana";
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -14,7 +17,18 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: Readonly<PropsWithChildren>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {' '}
+        <DynamicContextProvider
+          settings={{
+            environmentId: '9a6dc4c8-71a6-47b7-a570-c111c5de1f2c',
+            walletConnectors: [SolanaWalletConnectors],
+          }}
+        >
+          <DynamicWidget />
+        </DynamicContextProvider>
+        {children}
+      </body>
     </html>
   );
 }
